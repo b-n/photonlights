2ddc69d6c849a54b11a885209550eb1afbf0dce8
Ben Naylor <nayben@gmail.com>
2015-09-07 17:44:59 +1200

    initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+	var accessToken;
+
+	$('#login').submit(function(e){
+
+		var username = $("#inputEmail").val();
+		var password = $("#inputPassword").val();
+
+		e.preventDefault();
+
+		$.ajax({
+		  	type: "POST",
+		  	url: "https://api.particle.io/oauth/token",
+		  	contentType: "application/x-www-form-urlencoded",
+		  	beforeSend: function(xhr) { xhr.setRequestHeader("Authorization", "Basic " + btoa("particle:particle")) },
+		  	data: 'username=' + username + '&password=' + password + '&grant_type=password',
+		  	success: function (data){
+    			console.log('Success!', data);
+    			accessToken = data.access_token;
+    			window.location.href = "/lights.html?token=" + accessToken;
+    		}
+		});
+	});
+
+
+	$.urlParam = function(name){
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		return results[1] || 0;
+	}
+
+	if ($('#blinky').length) {
+		var retrievedToken = $.urlParam("token");
+		var devices;
+
+		$.ajax({
+		  	type: "GET",
+		  	url: "https://api.particle.io/v1/devices?access_token=" + retrievedToken,
+		  	contentType: "application/x-www-form-urlencoded",
+		  	success: function (data){
+    			console.log('Success!', data);
+
+				$.each(data, function(i, v){
+
+					if (v.connected){
+						$('#devices').append("<option id='" + v.id + "'>" + v.name + "</option>");
+					}
+					
+				});
+    		}
+		});
+
+		$('#blinky').submit(function(e){
+
+			e.preventDefault();
+
+			var deviceId = $('#devices').children(":selected").attr("id");
+			var paramString = "";
+
+			$.each($('.led'), function(i, v){
+
+				var value = $('input:radio[name=led' + i + ']:checked').val();
+
+				if (value !== "nc") {
+					paramString += $('input:radio[name=led' + i + ']:checked').val();
+				}
+
+			});
+
+			$.ajax({
+			  	type: "POST",
+			  	url: "https://api.particle.io/v1/devices/" + deviceId + "/write?access_token=" + retrievedToken,
+			  	contentType: "application/x-www-form-urlencoded",
+			  	data: "params=" + paramString,
+			  	success: function (data){
+	    			console.log('Success!', data);
+	    		}
+			});
+
+
+		});
+
+
+
+	}
+	
+
+});
